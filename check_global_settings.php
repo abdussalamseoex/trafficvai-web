81aa5986bbd5835a6d6bd1b275fc82756fa8b4da
Abdus Salam <abdussalamseoex@gmail.com>
2026-03-13 13:17:55 +0600

    Remove unused debugging scripts

--- check_global_settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'vendor/autoload.php';
-$app = include 'bootstrap/app.php';
-$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-
-echo "gateway_crypto_enabled: " . (\App\Models\Setting::get('gateway_crypto_enabled', '0')) . PHP_EOL;
-echo "gateway_bd_enabled: " . (\App\Models\Setting::get('gateway_bd_enabled', '0')) . PHP_EOL;
-echo "gateway_plisio_enabled: " . (\App\Models\Setting::get('gateway_plisio_enabled', '0')) . PHP_EOL;
--- debug_gateways.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'vendor/autoload.php';
-$app = include 'bootstrap/app.php';
-$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-
-$gateways = \App\Services\Payments\PaymentGatewayManager::getEnabledGateways();
-echo json_encode($gateways, JSON_PRETTY_PRINT) . PHP_EOL;
