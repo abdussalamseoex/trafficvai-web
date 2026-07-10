f1d8f9d367f10a05982bebdbece7a2399ceb1969
Abdus Salam <abdussalamseoex@gmail.com>
2026-07-10 10:41:22 +0600

    Import TrafficPointLedger and TrafficCampaign classes in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Models\Invoice;
+use App\Models\TrafficCampaign;
+use App\Models\TrafficPointLedger;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
