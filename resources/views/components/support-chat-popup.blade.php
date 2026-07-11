2bc4a211acdda929b6fa870c11c651741eb921d7
Abdus Salam <abdussalamseoex@gmail.com>
2026-07-11 20:58:39 +0600

    Fix duplicate endif syntax error in support-chat-popup.blade.php

--- resources/views/components/support-chat-popup.blade.php
+++ resources/views/components/support-chat-popup.blade.php
@@ -445,12 +445,6 @@ class="fixed flex flex-col items-end" style="position: fixed !important; bottom:
             </a>
             @endif
         @endif
-    </div>            >
-                <span class="absolute right-full mr-3 top-1/2 -translate-y-1/2 whitespace-nowrap px-3 py-1.5 bg-gray-900 text-white text-xs font-bold rounded-xl shadow-2xl opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none z-50">Telegram Support</span>
-                <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24"><path d="M11.944 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0a12 12 0 0 0-.056 0zm4.962 7.224c.1-.002.321.023.465.14a.506.506 0 0 1 .171.325c.016.093.036.306.02.472-.18 1.898-.962 6.502-1.36 8.627-.168.9-.499 1.201-.82 1.23-.696.065-1.225-.46-1.9-.902-1.056-.693-1.653-1.124-2.678-1.8-1.185-.78-.417-1.21.258-1.91.177-.184 3.247-2.977 3.307-3.23.007-.032.014-.15-.056-.212s-.174-.041-.249-.024c-.106.024-1.793 1.14-5.061 3.345-.48.33-.913.49-1.302.48-.428-.008-1.252-.241-1.865-.44-.752-.245-1.349-.374-1.297-.789.027-.216.325-.437.893-.663 3.498-1.524 5.83-2.529 6.998-3.014 3.332-1.386 4.025-1.627 4.476-1.635z"/></svg>
-            </a>
-            @endif
-        @endif
     </div>
 
     <!-- Main Support Toggle Button -->
